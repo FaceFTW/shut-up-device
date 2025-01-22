821697036f1f43e79e9e044cbcb56c4f5d8a9e92
Alex Westerman <alex@faceftw.dev>
2025-01-21 20:11:05 -0500

    Panic Handling + optimize for size-ish

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,41 @@
 mod display;
 
 use display::SSD1306Display;
-use panic_halt as _;
+// use panic_halt as _;
 pub use unwrap_infallible::UnwrapInfallible as _;
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    // disable interrupts - firmware has panicked so no ISRs should continue running
+    avr_device::interrupt::disable();
+
+    // get the peripherals so we can access serial and the LED.
+    //
+    // SAFETY: Because main() already has references to the peripherals this is an unsafe
+    // operation - but because no other code can run after the panic handler was called,
+    // we know it is okay.
+    let dp = unsafe { arduino_hal::Peripherals::steal() };
+    let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    // Print out panic location
+    ufmt::uwriteln!(&mut serial, "Firmware panic!\r").unwrap_infallible();
+    if let Some(loc) = info.location() {
+        ufmt::uwriteln!(
+            &mut serial,
+            "  At {}:{}:{}\r",
+            loc.file(),
+            loc.line(),
+            loc.column(),
+        )
+        .unwrap_infallible();
+    }
+
+    // Blink LED rapidly
+    let mut led = pins.d13.into_output();
+    led.set_high();
+    loop {} //So it's infallible type ret.
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
